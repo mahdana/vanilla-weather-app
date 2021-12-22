d02cca33d865c93d54e8e37b0a473271009f3c78
mahdana <95429443+mahdana@users.noreply.github.com>
2021-12-22 13:26:40 +0330

    update time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours=`0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return`${day} ${hours}:${minutes}`
+    
+}
+
+
+
 function showTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML= Math.round(response.data.main.temp);
@@ -9,6 +27,8 @@ function showTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "d6dead253c83cdc20a4af9f0a7e2f201";

d06d73531284f76e0edc810180ebbb1af4a07eb5
mahdana <95429443+mahdana@users.noreply.github.com>
2021-12-22 12:56:50 +0330

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 function showTemperature(response) {
-    console.log(response);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 let apiKey = "d6dead253c83cdc20a4af9f0a7e2f201";
 let city = "New York";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);

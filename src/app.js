d28e1b9eabc740aed25542ac7e2c058f4d0dc1fc
mahdana <95429443+mahdana@users.noreply.github.com>
2021-12-27 12:30:48 +0330

    updated JS for forecast

--- src/app.js
+++ src/app.js
@@ -70,4 +70,28 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
+
+
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML +
+            `        <div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+                </div>
+                <img src="https://openweathermap.org/img/wn/04n@2x.png" alt="" width="54">
+           <div class="weather-forecast-temperature">
+               <span class="weather-forecast-temperature-max">18°</span>
+             <span class="weather-forecast-temperature-min">12°</span>
+</div>
+            </div>`});
+        
+    forecastHTML = forecastHTML + `</div>`
+    forecastElement.innerHTML = forecastHTML
+};
+    
 search("New York");
+displayForecast();
